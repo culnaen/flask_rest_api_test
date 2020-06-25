6119bc08c52586650df9b17477c5eacd26612356
culnaen <culnaen@protonmail.com>
2020-06-25 19:38:14 +0300

    add type hints

--- app/views.py
+++ app/views.py
@@ -1,23 +1,26 @@
+from typing import Union, Dict, Tuple
+
 from flask import request, jsonify
 from flask.views import MethodView
 
 from .database import UserRepository
 from .model import User
 
+UserResponse = Tuple[Union[str, Dict[str, str]], int]
+
 
 class UserView(MethodView):
     def __init__(self, user_repository: UserRepository):
-        self.endpoint = "user"
         self.user_repository = user_repository
 
-    def get(self, user_id):
+    def get(self, user_id: str) -> UserResponse:
         if user_id is None:
-            result = jsonify(self.user_repository.get_all())
+            result = jsonify(self.user_repository.get_all()), 200
         else:
-            result = jsonify(self.user_repository.get(user_id))
+            result = jsonify(self.user_repository.get(user_id)), 200
         return result
 
-    def post(self):
+    def post(self) -> UserResponse:
         data = request.get_json()
         if data:
             try:
@@ -29,15 +32,15 @@ class UserView(MethodView):
             result = jsonify("params not specified"), 400
         return result
 
-    def delete(self, user_id):
-        return jsonify(self.user_repository.delete(user_id))
+    def delete(self, user_id: str) -> UserResponse:
+        return jsonify(self.user_repository.delete(user_id)), 200
 
-    def put(self, user_id):
+    def put(self, user_id: str) -> UserResponse:
         data = request.get_json()
         if data:
             try:
                 user = User(user_id=user_id, **data)
-                result = self.user_repository.set(user)
+                result = self.user_repository.set(user), 200
             except TypeError:
                 result = jsonify("invalid params"), 400
         else:
