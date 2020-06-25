32f7391be2093ca37c7d9dd95ad8bb3c95afd452
culnaen <culnaen@protonmail.com>
2020-06-25 23:26:01 +0300

    update id generation method

--- app/database.py
+++ app/database.py
@@ -17,10 +17,18 @@ class UserRepository:
         with open(self.db, "w") as f:
             json.dump(data, f)
 
+    def make_id(self):
+        with open("count") as f:
+            count = int(f.readline().strip()) + 1
+        count = str(count)
+        with open("count", "w") as f:
+            f.write(count)
+        return count
+
     def set(self, user: User) -> Union[str, Dict[str, str]]:
         users = self._read()
         if user.user_id is None:
-            user_id = str(len(users) + 1)
+            user_id = self.make_id()
             result = self._set(users, user_id, user.name)
         else:
             result = self._update(users, user.user_id, user.name)
